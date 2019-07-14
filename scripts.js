f5b83e0aa06fcbca49a6c0699f4c36ba5b70289f
kumahacker <kumahavana@gmail.com>
2019-07-13 21:55:21 -0700

    update

--- scripts.js
+++ scripts.js
@@ -1,73 +1,73 @@
 function showToast(text) {
-  M.toast({html: text});
+    M.toast({html: text});
 }
 
 function msgLengthValidate() {
-  var msg = $('#message').val().trim();
-  if (msg.length <= 160) {
-    $('.helper-text').html('Restante: ' + (160 - msg.length));
-  }
-  else {
-    $('.helper-text').html('Limite excedido');
-  }
+    var msg = $('#message').val().trim();
+    if (msg.length <= 160) {
+        $('.helper-text').html('Restante: ' + (160 - msg.length));
+    }
+    else {
+        $('.helper-text').html('Limite excedido');
+    }
 }
 
 function send() {
-  var number = $('#country').val() + $('#number').val();
-  var message = $('#message').val();
-  var cellphone = '';
+    var number = $('#country').val() + $('#number').val();
+    var message = $('#message').val();
+    var cellphone = '';
 
-  if ($("#cellphone").length){
-    cellphone = $("#cellphone").val();
-    if (!cellphone) {
-      showToast('Díganos su número de celular');
-      return false;
+    if ($("#cellphone").length) {
+        cellphone = $("#cellphone").val();
+        if (!cellphone) {
+            showToast('Díganos su número de celular');
+            return false;
+        }
     }
-  }
 
-  if (!number) {
-    showToast('Díganos el número de celular');
-    return false;
-  }
+    if (!number) {
+        showToast('Díganos el número de celular');
+        return false;
+    }
 
-  if (!message) {
-    showToast('Escriba un mensaje');
-    return false;
-  }
+    if (!message) {
+        showToast('Escriba un mensaje');
+        return false;
+    }
 
-  apretaste.send({
-    command: "SMS",
-    data: {
-      cellphone: cellphone,
-      number: number,
-      message: message
-    },
-    redirect: true
-  });
+    apretaste.send({
+        command: "SMS",
+        data: {
+            cellphone: cellphone,
+            number: number,
+            message: message
+        },
+        redirect: true
+    });
 }
 
-$(function(){
-  $("#country").formSelect();
+$(function () {
+    $("#country").formSelect();
 
-  $("#cellphone").keypress(function(e){
-    if (e.keyCode < 48 || e.keyCode > 57) {
-      e.preventDefault();
+    $("#cellphone")
+        .keypress(function (e) {
+            if (e.keyCode < 48 || e.keyCode > 57) {
+                e.preventDefault();
+                return false;
+            }
+        }).keyup(function (e) {
+        if ($("#cellphone").val().length >= 10) {
+            $("#cell-number-section").show().removeClass('hide');
+        }
+        else {
+            $("#cell-number-section").hide();
+        }
+    });
 
-      if ($("#cellphone").val().length >= 10)
-      {
-          $("#cell-number-section").show().removeClass('hide');
-      } else {
-          $("#cell-number-section").hide();
-      }
-
-      return false;
-    }
-  });
-
-  $("#number").keypress(function(e){
-    if (e.keyCode < 48 || e.keyCode > 57) {
-      e.preventDefault();
-      return false;
-    }
-  });
+    $("#number").keypress(function (e) {
+        if (e.keyCode < 48 || e.keyCode > 57) {
+            e.preventDefault();
+            return false;
+        }
+    });
 });
