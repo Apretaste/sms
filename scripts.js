b212968c2c17e5a464cf3004fb4f880cd4789666
kumahacker <kumahavana@gmail.com>
2019-08-11 22:35:57 -0400

    update

--- scripts.js
+++ scripts.js
@@ -1,93 +1,99 @@
 function msgLengthValidate() {
-    var msgControl = $('#message');
-    var msg = msgControl.val().trim();
-    if (msg.length <= 160) {
-        $('.helper-text').html(msg.length + '/160');
-        msgControl.addClass('valid');
-        msgControl.removeClass('invalid');
-    }
-    else {
-        $('.helper-text').html('Limite excedido');
-        msgControl.addClass('invalid');
-        msgControl.removeClass('valid');
-        showToast('El mensaje no puede exceder los 160 caracteres.');
-    }
+  var msgControl = $('#message');
+  var msg = msgControl.val().trim();
+  if (msg.length <= 160) {
+    $('.helper-text').html(msg.length + '/160');
+    msgControl.css('color', 'black');
+    msgControl.addClass('valid');
+    msgControl.removeClass('invalid');
+  }
+  else {
+    $('.helper-text').html('Limite excedido');
+    msgControl.css('color', 'red');
+    msgControl.addClass('invalid');
+    msgControl.removeClass('valid');
+    showToast('El mensaje no puede exceder los 160 caracteres.');
+  }
 }
 
 function showToast(text) {
-    M.toast({html: text});
+  $(".toast").remove();
+  M.toast({html: text});
 }
 
 function msgLengthValidateNumber() {
-    var msg = $('#number').val().trim();
-    var helper = $('.helper-text-number');
-    if (msg.length < 8) {
-        helper.html('Al menos ' + (8 - msg.length) + ' d&iacute;gitos');
-    } else helper.html('');
+  var msg = $('#number').val().trim();
+  var helper = $('.helper-text-number');
+  if (msg.length < 8) {
+    helper.html('Al menos ' + (8 - msg.length) + ' d&iacute;gitos');
+  }
+  else {
+    helper.html('');
+  }
 }
 
 function sendCellphone() {
-    var cellphone = $("#cellphone").val();
-    if (!cellphone) {
-        showToast('Díganos su número de celular');
-        return false;
-    }
+  var cellphone = $("#cellphone").val();
+  if (!cellphone) {
+    showToast('Díganos su número de celular');
+    return false;
+  }
 
-    apretaste.send({
-        command: "SMS_PROFILE",
-        data: {
-            cellphone: cellphone
-        },
-        redirect: true
-    });
+  apretaste.send({
+    command: "SMS_PROFILE",
+    data: {
+      cellphone: cellphone
+    },
+    redirect: true
+  });
 }
 
 function send() {
-    var number = $('#number').val();
-    var message = $('#message').val();
+  var number = $('#number').val();
+  var message = $('#message').val();
 
-    if (number.length < 8) {
-        showToast('Díganos el número de celular. Debe tener al menos 8 d&iacute;gitos.');
-        return false;
-    }
+  if (number.length < 8) {
+    showToast('Díganos el número de celular. Debe tener al menos 8 d&iacute;gitos.');
+    return false;
+  }
 
-    if (message.length > 160) {
-        showToast('El mensaje no debe exceder los 160 caracteres');
-        return false;
-    }
+  if (message.length > 160) {
+    showToast('El mensaje no debe exceder los 160 caracteres');
+    return false;
+  }
 
-    number = $('#country').val() + number;
+  number = $('#country').val() + number;
 
-    if (!message) {
-        showToast('Escriba un mensaje');
-        return false;
-    }
+  if (!message) {
+    showToast('Escriba un mensaje');
+    return false;
+  }
 
-    apretaste.send({
-        command: "SMS",
-        data: {
-            number: number,
-            message: message
-        },
-        redirect: true
-    });
+  apretaste.send({
+    command: "SMS",
+    data: {
+      number: number,
+      message: message
+    },
+    redirect: true
+  });
 }
 
 $(function () {
-    $("#country").formSelect();
+  $("#country").formSelect();
 
-    $("#cellphone")
-        .keypress(function (e) {
-            if (e.keyCode < 48 || e.keyCode > 57) {
-                e.preventDefault();
-                return false;
-            }
-        });
-
-    $("#number").keypress(function (e) {
+  $("#cellphone")
+      .keypress(function (e) {
         if (e.keyCode < 48 || e.keyCode > 57) {
-            e.preventDefault();
-            return false;
+          e.preventDefault();
+          return false;
         }
-    });
+      });
+
+  $("#number").keypress(function (e) {
+    if (e.keyCode < 48 || e.keyCode > 57) {
+      e.preventDefault();
+      return false;
+    }
+  });
 });
